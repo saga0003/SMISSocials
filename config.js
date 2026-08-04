4ea310dafe892a049ae8f3a220b3bda122cbea59
saga0003 <sagar@smis.edu.in>
2026-08-04 05:53:23 +0530

    Connect production Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SMIS_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_PUBLISHABLE_KEY: "",
-  DEMO_MODE: true
+  SUPABASE_URL: "https://ozgcbqjsduzcaacbbwmf.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_ipbuZMMRIlKc5i3litk7Gw_gZu4w__W",
+  DEMO_MODE: false
 };
